cce78373b8a3317ce9d7904cd7b3c434e0002659
Vijesh Srimal - 6143440 <6143440@fit2net.com>
2023-06-02 18:11:24 +0530

    shreyas

---
+++ Documents/MortgageFraudContexts/MortgageContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageFraudContexts
+{
+    public class MortgageContext : DbContext
+    {
+        public MortgageContext(DbContextOptions<MortgageContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MortgageContext> Buyer { get; set; } = null!;
+        public DbSet<MortgageContext> ValuationReport { get; set; } = null!;
+    }
+}
