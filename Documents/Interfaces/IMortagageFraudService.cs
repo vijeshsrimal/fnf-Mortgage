c920c1d81ebcde6a41dd8ec639266e12f01ee65d
Vijesh Srimal - 6143440 <6143440@fit2net.com>
2023-06-02 18:09:02 +0530

    secodn commit

---
+++ Documents/Interfaces/IMortagageFraudService.cs
@@ -0,0 +1,15 @@
+﻿using MortgageFraudsDetectionModels;
+using MortgageFraudsDetectionModels.Models;
+
+namespace Interfaces
+{
+    public interface IMortagageFraudService
+    {
+        Task<string> PurchasePower();
+        Task<string> CheckMarketValue();
+
+        Task<bool> CheckLawyerOpinion();
+
+        //Task<IEnumerable<ValuationReport>> GetProduct();
+    }
+}
---
+++ Documents/Services/MortgageFraudService.cs
@@ -0,0 +1,51 @@
+﻿using Interfaces;
+using MortgageFraudContexts;
+using Services;
+using System.Runtime.InteropServices;
+
+namespace Services
+{
+    public class MortgageFraudService : IMortagageFraudService
+    {
+        public async Task<string> PurchasePower()
+        {
+            string result = "";
+            double perMonthSalary = 50000;
+            double mortgageLoanRequested = 3600000;
+
+            double ProximateResult = perMonthSalary * 0.40 * 15 * 12;
+            double UppermortgageLoanRequested = mortgageLoanRequested + 200000;
+            double lowermortgageLoanRequested = mortgageLoanRequested - 200000;
+            if (ProximateResult >= lowermortgageLoanRequested && ProximateResult <= UppermortgageLoanRequested)
+                result = "Yellow";
+
+            if (ProximateResult > UppermortgageLoanRequested)
+                result = "Green";
+
+            if (ProximateResult < lowermortgageLoanRequested)
+                result = "Green";
+
+            return result;
+        }
+
+        public async Task<string> CheckMarketValue()
+        {
+
+            decimal marketValue = 700000;
+            decimal quotedAmount = 1000000;
+            decimal bankOffer = marketValue * 1.7m;
+            string color = quotedAmount > bankOffer ? "Red" : "Green";
+            return color;
+        }
+        
+        public async Task<bool> CheckLawyerOpinion()
+        {
+            string lawyer_opinion = "Approved";
+
+            if (lawyer_opinion == "Approved")
+                return true;
+            else
+                return false;
+        }
+    }
+}
